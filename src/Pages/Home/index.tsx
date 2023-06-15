0e5aa1a705083d3d8c6231fed5b8d197075d59ce
Ajinkya Patil <ajinkyap331@gmail.com>
2023-06-15 14:02:21 +0530

    first commit

---
+++ src/Pages/Home/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className = "text-white bg-slate-800 h-screen w-screen">
+      <div className='text-xl'>Home Screen</div>
+    </div>
+  )
+}
+
+export default Home
+
